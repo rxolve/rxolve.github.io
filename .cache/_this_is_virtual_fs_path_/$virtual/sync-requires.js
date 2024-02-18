a29f29b9ee523bb74c317a8ae1cf344669d50ac0
rxolve <rxolve@gmail.com>
2024-02-18 21:18:07 +0900

    set gatsby-starter-blog

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/help/Desktop/workswhy/rxolve.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/help/Desktop/workswhy/rxolve.github.io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/help/Desktop/workswhy/rxolve.github.io/src/pages/index.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/help/Desktop/workswhy/rxolve.github.io/src/pages/using-typescript.tsx")),
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/help/Desktop/workswhy/rxolve.github.io/src/templates/blog-post.js"))
+}
+
