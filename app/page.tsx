6b4b06c3006cd8c25091e79bb15158f133f606c6
help <rxolve@gmail.com>
2024-05-06 18:38:11 +0900

    remove: tailwind files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        Hello
-      </div>
+    <main>
+      <div>Hello</div>
+      <input type="text" />
     </main>
   );
 }
