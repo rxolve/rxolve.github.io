7e779f01d6b186c0bb7f3aa301f109332089f03f
help <rxolve@gmail.com>
2024-05-07 19:23:30 +0900

    blog: apply pico.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Page() {
   return (
-    <main>
-      <div>Hello</div>
-      <input type="text" />
+    <main className="container-fluid">
+      <h2>Hello, Stranger ☕</h2>
+      <h1>Welcome to Rxolve's blog</h1>
+      <form className="pico">
+        <input type="text" name="text" placeholder="Text" aria-label="Text" />
+      </form>
     </main>
   );
 }
