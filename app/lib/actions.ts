0c87c30145ee45850ce20316775caf1c6c0c7cd6
help <rxolve@gmail.com>
2024-05-08 19:18:00 +0900

    blog: add server-actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,12 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+
+export async function createText(formData: FormData) {
+  const text: string = formData.get('text') as string;
+
+  await sql`
+    INSERT INTO test (text)
+    VALUES (${text})
+    `;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { createText } from './lib/actions';
+
 export default function Page() {
   return (
     <main className="container-fluid">
       <h2>Hello, Stranger ☕</h2>
-      <h1>Welcome to Rxolve's blog</h1>
-      <form className="pico">
+      <form className="pico" action={createText}>
         <input type="text" name="text" placeholder="Text" aria-label="Text" />
+        <button type="submit">Submit</button>
       </form>
     </main>
   );
