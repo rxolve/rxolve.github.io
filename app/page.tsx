e1dcb6c56e04b3c2e9e0645f34928c3c7daaf3ba
help <rxolve@gmail.com>
2024-05-20 19:31:46 +0900

    update main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Page() {
   return (
     <main>
-      <div className="title">
+      <article>
         <h1>Hello, Stranger ☕</h1>
         <p>이뢰뵈도 12년차 개발자 블로그입니다.</p>
-      </div>
+      </article>
     </main>
   );
 }
