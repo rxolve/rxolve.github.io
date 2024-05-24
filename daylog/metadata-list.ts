9b48e0f7986e0673e00e82831a68778cb20f02a1
help <rxolve@gmail.com>
2024-05-24 18:36:18 +0900

    considering metadata-list format

---
+++ daylog/metadata-list.ts
@@ -0,0 +1,10 @@
+export const metadataList = [
+  {
+    date: '240101',
+    title: '2439 다이어리',
+  },
+  {
+    date: '240102',
+    title: '배운게 백엔드',
+  },
+];
--- daylog/metadata-list.tsx
+++
@@ -1,9 +0,0 @@
-const MetadataList = () => {
-  return (
-    <div>
-      <h1>Metadata List</h1>
-    </div>
-  );
-};
-
-export default MetadataList;
