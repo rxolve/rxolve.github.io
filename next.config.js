3f90c8f07905160c9cbda0712f28a1f948a5f539
help <rxolve@gmail.com>
2024-06-06 13:31:32 +0900

    remove asset path in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,9 @@ const withMDX = createMDX({
   extension: /\.mdx?$/,
 });
 
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/rxolve.github.io' : '',
-  assetPrefix: isProd ? '/rxolve.github.io/' : '',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true,
