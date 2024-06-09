8a1b7b63b4101960a1a192c8395a8cfb97e26990
help <rxolve@gmail.com>
2024-06-09 15:48:54 +0900

    stub: tidy-first-1.mdx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
       <article>
-        <h1>Resolve to solve 🤔</h1>
+        <h1>Rxolve to solve 🤔</h1>
         <h6>12년차 개발자의 그저 그렇고 그런 기록 ✍</h6>
         <div>
           <small>Made with Next.js, Vercel, Pico CSS, MDX, and GPT-4o 🚀</small>
