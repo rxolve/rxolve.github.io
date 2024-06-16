7623d488f64a92266c2735eacf0da2b554c6e42e
help <rxolve@gmail.com>
2024-06-17 00:14:39 +0900

    feat: add email in main

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,15 @@ const Home = () => {
         <h1>Rxolve to solve 🤔</h1>
         <h6>12년차 개발자의 그저 그렇고 그런 기록 ✍</h6>
         <div>
-          <small>Made with Next.js, Vercel, Pico CSS, MDX, and GPT-4o 🚀</small>
+          <small>
+            Made with Next.js, Pico CSS, MDX, Github Pages and GPT-4o 🚀
+          </small>
+        </div>
+        <div>
+          💌
+          <a target="_blank" href="mailto:rxolve@gmail.com">
+            <small style={{ margin: "0 0.3rem" }}>rxolve@gmail.com</small>
+          </a>
         </div>
       </article>
       <TagList list={tagList} />
