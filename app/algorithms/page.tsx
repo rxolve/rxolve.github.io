6b569590077dfc87db3f65741d743c8a2475d927
rxolve <rxolve@gmail.com>
2024-07-28 20:54:03 +0900

    daylog: update 240608

--- app/algorithms/page.tsx
+++ app/algorithms/page.tsx
@@ -1,25 +1,83 @@
 const Algorithms = () => {
   const input = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-  const dp = new Array(input.length).fill(0);
-  const kadane = (arr: number[]) => {
-    dp[0] = arr[0];
-    dp[1] = arr[0] + arr[1] > arr[1] ? arr[0] + arr[1] : arr[1];
-    for (let i = 2; i < arr.length; i++) {
-      dp[i] = dp[i - 1] + arr[i] > arr[i] ? dp[i - 1] + arr[i] : arr[i];
-    }
-    return Math.max(...dp);
+  const timSort = (array: number[]): number[] => {
+    const MIN_MERGE = 64;
+
+    const merge = (left: number[], right: number[]): number[] => {
+      const result: number[] = [];
+      let leftIndex = 0;
+      let rightIndex = 0;
+
+      // 두 배열을 비교하여 작은 값부터 결과 배열에 추가합니다.
+      while (leftIndex < left.length && rightIndex < right.length) {
+        const leftItem = left[leftIndex];
+        const rightItem = right[rightIndex];
+
+        if (leftItem < rightItem) {
+          result.push(leftItem);
+          leftIndex += 1;
+        } else {
+          result.push(rightItem);
+          rightIndex += 1;
+        }
+      }
+
+      // 남은 요소들을 결과 배열에 추가합니다.
+      return result
+        .concat(left.slice(leftIndex))
+        .concat(right.slice(rightIndex));
+    };
+
+    const insertionSort = (arr: number[], left: number, right: number) => {
+      for (let i = left + 1; i <= right; i++) {
+        let temp = arr[i];
+        let j = i - 1;
+        while (j >= left && arr[j] > temp) {
+          arr[j + 1] = arr[j];
+          j--;
+        }
+        arr[j + 1] = temp;
+      }
+    };
+
+    const timSort = (arr: number[], n: number) => {
+      for (let i = 0; i < n; i += MIN_MERGE) {
+        insertionSort(arr, i, Math.min(i + MIN_MERGE - 1, n - 1));
+      }
+
+      for (let size = MIN_MERGE; size < n; size = 2 * size) {
+        for (let left = 0; left < n; left += 2 * size) {
+          let mid = Math.min(left + size - 1, n - 1);
+          let right = Math.min(left + 2 * size - 1, n - 1);
+
+          if (mid < right) {
+            const leftPart = arr.slice(left, mid + 1);
+            const rightPart = arr.slice(mid + 1, right + 1);
+            const mergedPart = merge(leftPart, rightPart);
+            for (let i = 0; i < mergedPart.length; i++) {
+              arr[left + i] = mergedPart[i];
+            }
+          }
+        }
+      }
+    };
+
+    timSort(array, array.length);
+
+    return array;
   };
 
   return (
     <div>
       <h1>Algorithms</h1>
-      <h2>Kadane's Algorithm</h2>
+      <h2>Tim Sort</h2>
       <p>
-        Given an integer array nums, find the contiguous subarray (containing at
-        least one number) which has the largest sum and return its sum.
+        <strong>Input:</strong> {input.join(", ")}
       </p>
-      {kadane(input)}
+      <p>
+        <strong>Output:</strong> {timSort(input).join(", ")}
+      </p>
     </div>
   );
 };
