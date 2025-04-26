7a545b38d03f55edc8f865ad288ac006a8178f20
Help <rxolve@gmail.com>
2025-04-26 12:00:00 +0900

    fix: build error

--- next.config.js
+++ next.config.js
@@ -8,9 +8,13 @@ const nextConfig = {
   output: "export",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     unoptimized: true,
   },
   reactStrictMode: false,
+  distDir: "out",
+  trailingSlash: true,
 };
 
 export default withMDX(nextConfig);
